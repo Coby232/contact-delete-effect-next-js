6f06e96ce83d13a9431c52d4cbdac1144b510cc3
Coby232 <selormkn2003@gmail.com>
2023-09-28 17:48:32 +0000

    new update

---
+++ app/components/DarkMode.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DarkMode = () => {
+  return (
+    <div className=' min-h-screen flex flex-row justify-center items-center h-32'>
+      <p className='text-4xl dark:text-blue-800'>Hello</p>
+    </div>
+  )
+}
+
+export default DarkMode
--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -2,12 +2,14 @@
 import React, { useState } from "react";
 import Effect from "./effect";
 import SocialMediaFix from "./socialMediaFix";
+import DarkMode from "./DarkMode";
 
 const HomePage = () => {
   return (
     <div className='w-screen' >
-      <Effect />
-      <SocialMediaFix/>
+      {/* <Effect />
+      <SocialMediaFix/> */}
+      <DarkMode/>
     </div>
   );
 };
